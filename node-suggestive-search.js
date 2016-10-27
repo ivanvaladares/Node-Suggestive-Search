cfa8fd1205851d08262db58b8b714e78d60cda99
Ivan Valadares <ivanvaladares@hotmail.com>
2016-10-26 20:33:27 -0400

    Update node-suggestive-search.js

--- node-suggestive-search.js
+++ node-suggestive-search.js
@@ -12,12 +12,6 @@ var db;
 var _thisModule = this;
 var initialized = false;
 
-//roadmap
-// getSuggestedItems
-// loadJson by json string
-// validade input json
-// catalog
-
 exports.init = function (options) {
 
 	if (!options) {
