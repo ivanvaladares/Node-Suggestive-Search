6889d29847edcaf25b97cacb9e8256b22276140a
Ivan Valadares <ivanvaladares@hotmail.com>
2017-09-29 23:36:05 -0400

    Minors improvements

--- plugins/mongodb.js
+++ plugins/mongodb.js
@@ -1,169 +1,156 @@
 const mongo = require('mongodb').MongoClient;
+const util = require("util");
+const EventEmitter = require('events');
 
-let dbItems, dbWords;
+let DbDriver = class {
+        
+    constructor (options) {
 
-const init = (options) => {
-
-    return new Promise((resolve, reject) => {
-
-        mongo.connect(options.mongoDatabase).then((database) => {
+        mongo.connect(options.mongoDatabase).then((database) => {            
 
             let itemsCollectionName = (options.itemsCollectionName !== undefined) ? options.itemsCollectionName : 'node-suggestive-search-items'; 
             let wordsCollectionName = (options.wordsCollectionName !== undefined) ? options.wordsCollectionName : 'node-suggestive-search-words';         
 
-            dbItems = database.collection(itemsCollectionName);
-            dbWords = database.collection(wordsCollectionName);
+            this.dbItems = database.collection(itemsCollectionName);
+            this.dbWords = database.collection(wordsCollectionName);
                         
-            resolve();
+            this.emit('initialized');
 
-        }).catch(err => {
-
-            reject(err);
-            
         });
+
+        return this;
         
-    });
-};
-
-const createWordObject = (word, cleanWord, soundexWord) => {
-    
-    let objWord = { word, cleanWord, soundex: soundexWord, items: {} };
-    
-    for (let i = 2; i <= cleanWord.length && i <= 4; i++) {
-        objWord[`p${i}i`] = cleanWord.substr(0, i).toLowerCase();
-        objWord[`p${i}e`] = cleanWord.substr(cleanWord.length - i, cleanWord.length).toLowerCase();
     }
 
-    return objWord;
-};
-
-const createItemObject = (itemId, itemName, keywords) => {
-
-    let objItem = { itemId, itemName };
-
-    if (keywords !== undefined){
-        objItem.keywords = keywords;
-    }
-
-    return objItem;
-};
-
-const insert = (collection, entry) => {
-    return new Promise((resolve, reject) => {
-        collection.insert(entry, { w: 0 }, (err, newDoc) => {
-            if (err) { return reject(err); }
-
-            resolve(newDoc);
-        });
-    });
-};
-
-const find = (collection, criteria) => {
-    return new Promise((resolve, reject) => {
-        collection.find(criteria).toArray((err, items) => {
-            if (err) { return reject(err); }
-            
-            resolve(items);
-        });
-    });    
-};
-
-const remove = (collection, criteria1, criteria2) => {
-    return new Promise((resolve, reject) => {
-        collection.remove(criteria1, criteria2, (err, numRemoved) => {
-            if (err) { return reject(err); }
-
-            resolve(numRemoved);
-        });
-    });    
-};
-
-const update = (collection, criteria, data, multi) => {
-    return new Promise((resolve, reject) => {
-        collection.update(criteria, data, multi, (err, numUpdated) => {
-            if (err) { return reject(err); }
-
-            resolve(numUpdated);
-        });
-    });    
-};
-
-const cleanDatabase = () => {
-    return new Promise((resolve, reject) => {
-        let p1 = remove(dbItems, {}, { multi: true });
-        let p2 = remove(dbWords, {}, { multi: true });
-
-        Promise.all([p1, p2]).then(() => {
-            resolve();
-        }).catch(err => {
-            reject(err);
-        });
-    });
-};
-
-const createIndexes = () => {   
-    return new Promise((resolve) => {
-
-        dbItems.createIndex({ 'itemId': 2 }, { unique: true });
-        dbWords.createIndex({ 'word': 1 }, {  unique: true });
-        dbWords.createIndex({ 'cleanWord': 2 }, { unique: false });
-        dbWords.createIndex({ 'soundex': 1 }, { unique: false });
-
-        for (let i = 2; i <= 4; i++) {
-            let ixi = {};
-            let ixe = {};
-            ixi[`p${i}i`] = 1;
-            ixe[`p${i}e`] = 1;
-            dbWords.createIndex(ixi, { sparse: true });
-            dbWords.createIndex(ixe, { sparse: true });
+    createWordObject (word, cleanWord, soundexWord) {
+        
+        let objWord = { word, cleanWord, soundex: soundexWord, items: {} };
+        
+        for (let i = 2; i <= cleanWord.length && i <= 4; i++) {
+            objWord[`p${i}i`] = cleanWord.substr(0, i).toLowerCase();
+            objWord[`p${i}e`] = cleanWord.substr(cleanWord.length - i, cleanWord.length).toLowerCase();
         }
+    
+        return objWord;
+    };
+    
+    createItemObject (itemId, itemName, keywords) {
+    
+        let objItem = { itemId, itemName };
+    
+        if (keywords !== undefined){
+            objItem.keywords = keywords;
+        }
+    
+        return objItem;
+    };
+    
+    insert (collection, entry) {
+        return new Promise((resolve, reject) => {
+            collection.insert(entry, { w: 0 }, (err, newDoc) => {
+                if (err) { return reject(err); }
+    
+                resolve(newDoc);
+            });
+        });
+    };
+    
+    find (collection, criteria) {
+        return new Promise((resolve, reject) => {
+            collection.find(criteria).toArray((err, items) => {
+                if (err) { return reject(err); }
+                
+                resolve(items);
+            });
+        });    
+    };
+    
+    remove (collection, criteria1, criteria2) {
+        return new Promise((resolve, reject) => {
+            collection.remove(criteria1, criteria2, (err, numRemoved) => {
+                if (err) { return reject(err); }
+    
+                resolve(numRemoved);
+            });
+        });    
+    };
+    
+    update (collection, criteria, data, multi) {
+        return new Promise((resolve, reject) => {
+            collection.update(criteria, data, multi, (err, numUpdated) => {
+                if (err) { return reject(err); }
+    
+                resolve(numUpdated);
+            });
+        });    
+    };
+    
+    cleanDatabase () {
+        return new Promise((resolve, reject) => {
+            let p1 = this.remove(this.dbItems, {}, { multi: true });
+            let p2 = this.remove(this.dbWords, {}, { multi: true });
+    
+            Promise.all([p1, p2]).then(() => {
+                resolve();
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    };
+    
+    createIndexes () {   
+        return new Promise((resolve) => {
+    
+            this.dbItems.createIndex({ 'itemId': 2 }, { unique: true });
+            this.dbWords.createIndex({ 'word': 1 }, {  unique: true });
+            this.dbWords.createIndex({ 'cleanWord': 2 }, { unique: false });
+            this.dbWords.createIndex({ 'soundex': 1 }, { unique: false });
+    
+            for (let i = 2; i <= 4; i++) {
+                let ixi = {};
+                let ixe = {};
+                ixi[`p${i}i`] = 1;
+                ixe[`p${i}e`] = 1;
+                this.dbWords.createIndex(ixi, { sparse: true });
+                this.dbWords.createIndex(ixe, { sparse: true });
+            }
+    
+            resolve();
+        });
+    };
 
-        resolve();
-    });
-};
+    insertItem (entry) {
+        return this.insert(this.dbItems, entry);
+    };
+    
+    insertWord (entry) {
+        return this.insert(this.dbWords, entry);
+    };
+    
+    findItems (criteria) {
+        return this.find(this.dbItems, criteria);
+    };
+    
+    findWords (criteria) {
+        return this.find(this.dbWords, criteria);
+    };
+    
+    updateWord (criteria1, data, multi) {
+        return this.update(this.dbWords, criteria1, data, multi);
+    }
+    
+    removeItem (criteria1) {
+        return this.remove(this.dbItems, criteria1, { multi: false });
+    };
+    
+    removeWords (criteria1) {
+        return this.remove(this.dbWords, criteria1, { multi: false });
+    };
 
-exports.init = init;
-
-exports.createWordObject = (word, cleanWord, soundexWord) => {
-    return createWordObject(word, cleanWord, soundexWord);
 }
 
-exports.createItemObject = (itemId, itemName, keywords) => {
-    return createItemObject(itemId, itemName, keywords);
-}
+util.inherits(DbDriver, EventEmitter);
 
-exports.insertItem = (entry) => {
-    return insert(dbItems, entry);
+exports.init = (options) => {
+    return new DbDriver(options);
 };
-
-exports.insertWord = (entry) => {
-    return insert(dbWords, entry);
-};
-
-exports.findItems = (criteria) => {
-    return find(dbItems, criteria);
-};
-
-exports.findWords = (criteria) => {
-    return find(dbWords, criteria);
-};
-
-exports.updateWord = (criteria1, data, multi) => {
-    return update(dbWords, criteria1, data, multi);
-}
-
-exports.removeItem = (criteria1) => {
-    return remove(dbItems, criteria1, { multi: false });
-};
-
-exports.removeWords = (criteria1) => {
-    return remove(dbWords, criteria1, { multi: false });
-};
-
-exports.cleanDatabase = () => {
-    return cleanDatabase();
-};
-
-exports.createIndexes = () => {
-    return createIndexes();
-}
---
+++ test/names.js
@@ -0,0 +1,199 @@
+const mocha = require('mocha')
+const assert = require("assert");
+const describe = mocha.describe;
+const before = mocha.before;
+const it = mocha.it;
+
+let nss;
+
+describe('Test names.json -', () => {
+
+    before(done => {
+
+        //tests using nedb
+        nss = require('../index.js').init(
+            {
+                dataBase: "nedb",
+                neDbDataPath: "",
+                neDbInMemoryOnly: true
+            });
+
+        //tests using mongogdb
+        // nss = require('../index.js').init(
+        // {
+        //     dataBase: "mongodb", 
+        //     mongoDatabase: "mongodb://localhost:27017/nodeSugestiveSearchTest"
+        // });
+
+        //tests using ms-sql
+        // let nss = require('../index.js').init(
+        //     {
+        //         dataBase: "mssql",
+        //         dbConnection: {
+        //             host: '127.0.0.1',
+        //             username: "sa",
+        //             password: 'mssqlpass',
+        //             database: "test",        
+        //             dialect: 'mssql',
+        //             logging: false,            
+        //             dialectOptions: {
+        //                 requestTimeout: 60000,
+        //                 encrypt: true // Use this if you're on Windows Azure                
+        //             }
+        //         }   
+        //     });        
+        
+        //wait for the initialization process
+        nss.on("initialized", () => {
+            done();
+        });
+    });
+
+    it('load json file test.json', () => {
+        return nss.loadJson("test/names.json")
+            .then(data => {
+                assert(
+                    data != null &&
+                    data.words == 3452 &&
+                    data.items == 50003,
+                    "Could not load json file."
+                );
+            });
+    });
+
+    it('query for: ivan valadares', () => {
+        return nss.query("ivan valadares")
+            .then(data => {
+                assert(
+                    data != null &&
+                    data.words.length == 2 &&
+                    data.words[0] == "Ivan" &&
+                    data.words[1] == "Valadares" &&
+                    data.itemsId.length == 1 &&
+                    data.itemsId[0] == "49999",
+                    "Error on query for: ivan valadares"
+                );
+            });
+    });
+
+    it('insert item: 13 - VODKA ABSOLUTE', () => {
+        return nss.insertItem({ "itemName": "VODKA ABSOLUTE", "itemId": "13" })
+            .then(data => {              
+                assert(
+                    data != null &&
+                    data.timeElapsed >= 0,
+                    "Error on inserting item: 13 - VODKA ABSOLUTE"
+                );
+            });
+    });
+
+    it('query for: absolut', () => {
+        return nss.query("absolut")
+            .then(data => {
+                assert(
+                    data != null &&
+                    data.words.length == 1 &&
+                    data.itemsId.length == 1 &&
+                    data.itemsId[0] == "13",
+                    "Error on query for: absolut"
+                );
+            });
+    });
+
+    it('query for: "VODKA ABSOLUTE"', () => {
+        return nss.query("\"VODKA ABSOLUTE\"")
+            .then(data => {
+                assert(
+                    data != null &&
+                    data.words.length == 2 &&
+                    data.words[0] == "VODKA" &&
+                    data.words[1] == "ABSOLUTE" &&
+                    data.itemsId.length == 1 &&
+                    data.itemsId[0] == "13",
+                    "Error on query for: \"VODKA ABSOLUTE\""
+                );
+            });
+    });    
+
+    it('remove item 13', () => {
+        return nss.removeItem("13")
+            .then(data => {
+                assert(
+                    data != null &&
+                    data.timeElapsed >= 0,
+                    "Error on removing item 13"
+                );
+            });
+    });
+
+    it('query for: "VODKA ABSOLUTE"', () => {
+        return nss.query("\"VODKA ABSOLUTE\"")
+            .then(data => {               
+                assert(
+                    data != null &&
+                    data.words.length == 2 &&
+                    data.words[0] == null &&
+                    data.words[1] == null &&
+                    data.itemsId.length == 0,
+                    "Error on query for: \"VODKA ABSOLUTE\""
+                );
+            });
+    });
+
+    it('query for: absolut', () => {
+        return nss.query("absolut")
+            .then(data => {
+                assert(
+                    data != null &&
+                    data.words.length == 1 &&
+                    data.words[0] == "Abshire" &&
+                    data.itemsId[0] == "611",
+                    "Error on query for: absolut"
+                );
+            });
+    });
+
+    it('query for: ivam vala', () => {
+        return nss.query("ivam vala")
+            .then(data => {
+                assert(
+                    data != null &&
+                    data.words.length == 2 &&
+                    data.words[0] == "Ivan" &&
+                    data.words[1] == "Valadares" &&
+                    data.itemsId.length == 1 &&
+                    data.itemsId[0] == "49999",
+                    "Error on query for: ivam vala"
+                );
+            });
+    });
+
+    it('get words suggestions for: ivan v', () => {
+        return nss.getSuggestedWords("ivan v")
+            .then(data => {
+                assert(
+                    data != null &&
+                    data.suggestions.length == 2 &&
+                    data.suggestions[0] == "Ivan Valadares" &&
+                    data.suggestions[1] == "Ivan Valentino",
+                    "Error on get words suggestions for: ivan v"
+                );
+            });
+    });
+
+    it('get items suggestions for: ivan v', () => {
+        return nss.getSuggestedItems("ivan v")
+            .then(data => {
+                assert(
+                    data != null &&
+                    data.items.length == 4 &&
+                    data.items[0].itemName == "Ivan Valadares" &&
+                    data.items[1].itemName == "Ivan SandoVal" &&
+                    data.items[2].itemName == "Ivan Campos" &&
+                    data.items[3].itemName == "Ivan Valentino",
+                    "Error on get items suggestions for: ivan v"
+                );
+            });
+    });
+
+});
